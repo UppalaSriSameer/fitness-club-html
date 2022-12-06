b74a04e1918e03a5d11c4f04cbe8b8f1be5fe9f2
UppalaSriSameer <119685104+UppalaSriSameer@users.noreply.github.com>
2022-12-06 22:43:27 +0530

    Add files via upload

---
+++ Gym Website/scrypt.js
@@ -0,0 +1,12 @@
+function hg1(){
+    document.getElementById("imgs").src="class-1.jpg";
+}
+function hg2(){
+    document.getElementById("imgs").src="class-2.jpg";
+}
+function hg3(){
+    document.getElementById("imgs").src="class-3.jpg";
+}
+function hg4(){
+    document.getElementById("imgs").src="class-4.jpg";
+}
